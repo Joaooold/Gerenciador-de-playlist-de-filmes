90de7eadcf12d2ee69545e31c5c53a6edfd330f4
Joao Batista <joaofilhot09@gmail.com>
2025-12-22 12:49:38 -0300

    quase finalizando projeto

--- modal.js
+++ modal.js
@@ -1,6 +1,48 @@
 const background = document.getElementById("modal-background");
+const modalConteiner = document.getElementById("modal-conteiner");
+
+let currentMovie = {};
 
 function backgroundClickHandler() {
   overlay.classList.remove("open-overlay");
+}
+
+function closeModal() {
+  overlay.classList.remove("open-overlay");
+}
+
+function addCurrentMovieToList() {
+  addToList(currentMovie);
+  updateUI(currentMovie);
+  closeModal()
+}
+function createModal(data) {
+  currentMovie = data;
+  modalConteiner.innerHTML = `
+  <h2 id="movie-title">${data.Title} - ${data.Year}</h2>
+          <section id="modal-body">
+            <img
+              id="movie-poster"
+              src=${data.Poster}
+              alt="poster do filme"
+            />
+            <div id="movie-info">
+              <h3 id="movie-plot">
+                ${data.Plot}
+              </h3>
+              <div id="movie-cast">
+                <h4>Elenco:</h4>
+                <h5>${data.Actors}</h5>
+              </div>
+              <div id="movie-genre">
+                <h4>Gênero:</h4>
+                <h5>${data.Genre}</h5>
+              </div> 
+            </div>
+          </section>
+          <section id="movie-footer">
+            <button id="add-to-list" onclick="{addCurrentMovieToList()}">Adicionar a lista</button>
+          </section>
+  `;
 }
 background.addEventListener("click", backgroundClickHandler);
--- script.js
+++ script.js
@@ -2,18 +2,24 @@ const searchButton = document.getElementById("search-button");
 const overlay = document.getElementById("modal-overlay");
 const movieName = document.getElementById("movie-name");
 const movieYear = document.getElementById("movie-year");
+const movieListConteiner = document.getElementById("movie-list");
+
+let movieList = [];
 
 async function searchButtonClickHandler() {
   try {
-    overlay.classList.add("open-overlay");
-
     let url = `http://www.omdbapi.com/?apikey=${key}&t=${movieNameParameterGenerator()}${movieYearParameterGenerator()}`;
     const response = await fetch(url);
     const data = await response.json();
     console.log("data: ", data);
-    console.log("response: ", response);
+
+    createModal(data);
+    overlay.classList.add("open-overlay");
+    if (data.Error) {
+      throw new Error("O filme não foi encontrado");
+    }
   } catch (error) {
-    console.error(error.message);
+    notie.alert({ type: "error", text: error.message });
   }
 }
 
@@ -25,10 +31,25 @@ function movieNameParameterGenerator() {
 }
 
 function movieYearParameterGenerator() {
+  if (movieYear.value === "") {
+    return "";
+  }
   if (movieYear.value.length !== 4 || Number.isNaN(Number(movieYear.value))) {
     throw new Error("O ano do filme estar inválido");
   }
   return `&y=${movieYear.value}`;
+}
+
+function addToList(movieObject) {
+  movieList.push(movieObject);
+}
+
+function updateUI(movieObject) {
+  movieListConteiner.innerHTML += `
+      <article>
+        <img src="${movieObject.Poster}" alt="Poster do filme ${movieObject.Title}">
+        <button class="remove-button"><i class="bi bi-trash"></i> Remover</button>
+      </article>`;
 }
 
 searchButton.addEventListener("click", searchButtonClickHandler);
